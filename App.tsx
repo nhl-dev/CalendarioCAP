96fc649acfda2885586a6709528ba1d212d7f6cb
Nahuel Caballero <nahuelcaballeroferme@gmail.com>
2023-04-26 17:47:34 -0300

    AsyncStorage & First Launch detection

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { storeData, getData } from './src/helpers/storageHelper';
+
+const HAS_LAUNCHED = 'HAS_LAUNCHED';
+
 export default function App() {
+  const [hasLaunched, setHasLaunched] = useState(false);
+
+  useEffect(() => {
+    const getAsyncData = async () => {
+      const hasLaunched = await getData(HAS_LAUNCHED);
+      if (hasLaunched) {
+        setHasLaunched(true);
+      } else {
+        await storeData(HAS_LAUNCHED, 'true');
+      }
+    };
+
+    getAsyncData().catch((e) => {
+      console.log(e);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {hasLaunched ? null : null}
     </View>
   );
 }
